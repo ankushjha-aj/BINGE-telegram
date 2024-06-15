564e75ef4eb76371e524562aa801af7f456c835c
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2024-06-15 17:28:37 +0530

    Update send_telegram_message.py

--- send_telegram_message.py
+++ send_telegram_message.py
@@ -52,7 +52,7 @@ def send_to_telegram(new_msg, prev_msg_file=prev_msg_file):
 
 # --- Main Execution ---
 # Replace this with the actual message URL
-new_msg = 'https://photos.app.goo.gl/abcdefg123fghfgh'
+new_msg = 'https://photos.app.goo.gl/abcdefg123fghfghfghh'
 
 send_to_telegram(new_msg, prev_msg_file)
 print('Script completed!')
