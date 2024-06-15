39bdaaf10491a2f63f5bbe10b6f7d420b5563523
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2024-06-15 13:39:51 +0530

    Update send_telegram_messages.py

--- scripts/send_telegram_messages.py
+++ scripts/send_telegram_messages.py
@@ -7,7 +7,8 @@ channel_id = os.environ.get('CHANNEL_ID')
 
 # Define multiple messages
 messages = [
-    'GAANTH --> https://photos.app.goo.gl/chxAPKxo3ecffK4KA',
+    'GAANTH -->',
+    'https://photos.app.goo.gl/chxAPKxo3ecffK4KA',
 ]
 
 # Create a Telegram bot object
